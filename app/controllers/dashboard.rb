0824c2619ddaf74ce773de48f1d6d996a0a1b70d
Kevin Huang & Jordan Fox <pair+daskevinhuang.jordanfox15@devbootcamp.com>
2016-02-11 16:49:58 -0800

    update controllers

--- app/controllers/dashboard.rb
+++ app/controllers/dashboard.rb
@@ -1,6 +1,20 @@
 
 # Displays tweets of the people that the user is following
 get '/dashboard' do
-
-erb :"/dashboard"
+  @user = User.find(id: session[:id])
+  @userfollowers = UserFollower.find_by(follower: @user)
+  @tweets = []
+  @userfollowers.each do |userfollower|
+    userfollower.user.tweets.each do |tweet|
+      @tweets << tweet
+  end
+  @tweets.sort {|first, second| first.created_at <=> second.created_at }
+  if logged_in?
+    erb :"/dashboard"
+  else
+    redirect '/'
+  end
 end
+
+
+
--- app/controllers/tweet.rb
+++ app/controllers/tweet.rb
@@ -1,13 +1,20 @@
 
 # To display all tweets of ALL users
 get '/tweets' do
+  @tweets = Tweet.all
 
+  erb :"/tweets/index"
 end
 
 # To create new tweet form
 get '/tweets/new' do
+  erb :'/tweets/new'
+end
 
-  if
+post '/tweets' do
+  @tweet = Tweet.new(body: params[:body], user_id: session[:id] )
+  if @tweet
+    @tweet.save
     redirect "/user/#{@user.id}"
   else
     redirect "/tweets/new"
